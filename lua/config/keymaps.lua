37e99f76bd3775d3c75aa31921e9dd38f6edfe18
Josh <joshua.8.kandel@gmail.com>
2026-05-10 19:57:45 +0200

    feat: add neotree keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,5 @@ vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height
 vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+vim.keymap.set("n", "<leader>e", "<Cmd>Neotree<CR>")
